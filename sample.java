8c5b2cfab81b1ceeae2127f3d4d86a254963d2ba
rahil <rahil@samsung.com>
2019-09-26 02:58:31 -0400

    code commited on 26th SEPT

---
+++ sample.java
@@ -0,0 +1,11 @@
+public class  sample {
+
+	public static void main(String args[])
+	{
+		System.out.println("Jenkins - Automation Server - Samsung ");
+		System.out.println("Developed and Managed by  - Rahil");
+		System.out.println("Destroyed  and Managed by - Rahil");
+
+	}
+}
+
